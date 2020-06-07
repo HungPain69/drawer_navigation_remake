a4c203e3b4d04ca4c353229d89e1b99aec6f4fc2
HungPain69 <hungpain69@gmail.com>
2020-06-07 17:12:49 +0700

    complete NavigationUI from NavView and NavController

--- app/src/main/java/com/example/drawernavigation/MainActivity.java
+++ app/src/main/java/com/example/drawernavigation/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.drawernavigation;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +26,11 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        NavController navController = Navigation.findNavController(this,R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
     }
 }
